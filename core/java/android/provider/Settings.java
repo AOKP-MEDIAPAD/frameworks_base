d2b6f424624e5246fee0345dc76d551ee78aa023
zyr3x <izzzum@gmail.com>
2013-04-18 07:31:28 +0300

    Option to set notification icon opacity 1/2 - Picked from CarbonRom
    Change-Id: If2f251b2187524b153a695977f3ee9ce5bf487db
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/statusbar/StatusBarIconView.java
    
    Conflicts:
    	core/java/android/provider/Settings.java
    	packages/SystemUI/src/com/android/systemui/statusbar/StatusBarIconView.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2410,6 +2410,12 @@ public final class Settings {
          */
         public static final String UI_FORCE_OVERFLOW_BUTTON = "ui_force_overflow_button";
 
+ 		/**
+         * Status Bar notification icon opacity
+         * @hide
+         */
+        public static final String STATUS_BAR_NOTIF_ICON_OPACITY = "status_bar_notif_icon_opacity";
+
         /**
          * AM/PM Style for clock options
          * 0 - Normal AM/PM
